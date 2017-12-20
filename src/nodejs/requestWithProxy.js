d1657673c1a2c3f03bb8826694085f2f366b1645
kule <kukulwe@163.com>
2017-12-20 14:37:25 +0800

    添加noproxy方法

--- src/nodejs/requestWithProxy.js
+++ src/nodejs/requestWithProxy.js
@@ -2,39 +2,82 @@
  * Created by kule on 2017/10/26.
  */
 import http from 'http';
+import https from 'https';
 import Url from 'url';
 import pick from 'lodash/pick';
 import {unGzipRes} from './unGzip';
 
+export const getDefaultHeaders=(headers={})=>{
+    return {
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding':'gzip',
+        'Accept-Language':'zh-CN,zh;q=0.8',
+        'Connection':'keep-alive',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+        ...headers
+    }
+};
 export const requestWithProxy=({
     url,
     headers={},
     method='get',
     body,
-    proxy={}
+    proxy={},
+    ...other
+}={})=>{
+    const urlObj=Url.parse(url);
+    const options={
+        method:method.toUpperCase(),
+        protocol:'http:',
+        host:proxy.host||'127.0.0.1',
+        path:Url.format(url),
+        port:proxy.port||8888,
+        headers:getDefaultHeaders({
+            Host:Url.format(pick(urlObj,['hostname','port'])),
+            ...headers
+        }),
+        body,
+        ...other
+    };
+    return requestWithOptions(options);
+};
+export const request=(options={})=>{
+    if(options.proxy){
+        return requestWithProxy(options);
+    }
+    return requestNoProxy(options);
+};
+export const requestNoProxy=({
+    url,
+    headers={},
+    method='get',
+    body,
+    ...other
+}={})=>{
+    const urlObj=Url.parse(url);
+    const options={
+        method:method.toUpperCase(),
+        ...pick(urlObj,['protocol','host','path','port']),
+        headers:getDefaultHeaders(headers),
+        body,
+        ...other
+    };
+    return requestWithOptions(options);
+};
+export const requestWithOptions=({
+    body,
+    ...options
 }={})=>{
     return new Promise((resolve,reject)=>{
-        const urlObj=Url.parse(url);
+        const _http=options.protocol=='https:'?https:http;
         if(body){
-            headers['Content-Type']='application/json;charset=UTF-8';
-            headers['Content-Length']=Buffer.byteLength(body);
+            options.headers={
+                'Content-Type':'application/json;charset=UTF-8',
+                'Content-Length':Buffer.byteLength(body),
+                ...(options.headers||{})
+            }
         }
-        const request=http.request({
-            method:method.toUpperCase(),
-            path:Url.format(url),
-            headers:{
-                'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                'Accept-Encoding':'gzip',
-                'Accept-Language':'zh-CN,zh;q=0.8',
-                'Connection':'keep-alive',
-                'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
-                Host:Url.format(pick(urlObj,['hostname','port'])),
-                ...headers
-            },
-            protocol:'http:',
-            host:proxy.host||'127.0.0.1',
-            port:proxy.port||8888
-        });
+        const request=_http.request(options);
         request.on('response',(res)=>{
             const {statusCode}=res;
             if(statusCode!==200){
@@ -56,4 +99,4 @@ export const requestWithProxy=({
         request.end();
     });
 };
-export default requestWithProxy;
+export default request;
