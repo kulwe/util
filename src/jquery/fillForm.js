c9f16a67d895308175baf475cb866b26edbebabf
kule <kukulwe@163.com>
2018-01-20 16:59:57 +0800

    修复bug

--- src/jquery/fillForm.js
+++ src/jquery/fillForm.js
@@ -1,5 +1,5 @@
 import inArray from '../inArray';
-const fillForm=(data)=>{
+const fillForm=(jqForm,data)=>{
     $('input',jqForm).each(function(index){
         const {type,name,value}=this;
         if(!(name in data)){
