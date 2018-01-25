6bac13d0b98274fe64e7ad5045458337be1fe801
kule <kukulwe@163.com>
2018-01-25 15:33:57 +0800

    曾倩countdown方法

--- src/countdown.js
+++ src/countdown.js
@@ -1,42 +1,68 @@
 import padStart from 'lodash/padStart';
-export const countdown=(count,fn,{
-    isPad=false
-}={})=>{
-    let end=(new Date).getTime()+count;
-    const hourTime=60*60*1000;
-    const minuteTime=60*1000;
-    const secondTime=1000;
-    const down=()=>{
-        const now=(new Date).getTime();
-        const _count=end-now;
-        let retain=_count;
 
-        const hour=Math.floor(_count/hourTime);
-        retain-=hour*hourTime;
-        const minute=Math.floor(retain/minuteTime);
-        retain-=minute*minuteTime;
-        const second=Math.floor(retain/secondTime);
-        let rst;
-        if(padStart){
-            rst={
-                count:_count,
-                hour:padStart(hour,2,'0'),
-                minute:padStart(minute,2,'0'),
-                second:padStart(second,2,'0')
+export const countdown = (count, fn, {
+    isPad = false
+} = {}) => {
+    let end = (new Date).getTime() + count;
+    const hourTime = 60 * 60 * 1000;
+    const minuteTime = 60 * 1000;
+    const secondTime = 1000;
+
+    let isPause = false;
+    let timeHandler;
+
+    const getCount = () => {
+        const now = (new Date).getTime();
+        const _count = end - now;
+        let retain = _count;
+
+        const hour = Math.floor(_count / hourTime);
+        retain -= hour * hourTime;
+        const minute = Math.floor(retain / minuteTime);
+        retain -= minute * minuteTime;
+        const second = Math.floor(retain / secondTime);
+        if (isPad) {
+            return {
+                count: _count,
+                hour: padStart(hour, 2, '0'),
+                minute: padStart(minute, 2, '0'),
+                second: padStart(second, 2, '0')
             }
-        }else{
-            rst={
-                count:_count,
-                hour,
-                minute,
-                second
-            };
         }
-        fn(rst);
-        if(now<end){
-            setTimeout(down,1000);
+        return {
+            count: _count,
+            hour,
+            minute,
+            second
         }
     };
-    return down;
+
+    const down = () => {
+        if (isPause) {
+            return;
+        }
+        const now=(new Date).getTime();
+        if (now > end) {
+            return;
+        }
+        const count = getCount();
+        fn(count);
+        timeHandler=setTimeout(down, 1000);
+    };
+
+    const pause=()=>{
+        isPause=true;
+        clearTimeout(timeHandler);
+    };
+    const resume=()=>{
+        isPause=false;
+        down();
+    };
+    return {
+        start:down,
+        stop:pause,
+        pause,
+        resume
+    }
 };
 export default countdown;
--- test/countdown.js
+++ test/countdown.js
@@ -1,6 +1,15 @@
 const {countdown}=require('../lib/countdown');
 
-const start=countdown(3*60*60*1000,({hour,minute,second})=>{
+const {start,pause,resume,stop}=countdown(3*60*60*1000,({hour,minute,second})=>{
     console.log(`${hour}:${minute}:${second}`);
 },{isPad:true});
 start();
+setTimeout(()=>{
+    pause();
+},5000);
+setTimeout(()=>{
+    resume();
+},10000);
+setTimeout(()=>{
+    stop();
+},2*10000);
