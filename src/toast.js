79ef144bc39cb9d2a52859926255e25e7101af81
kule <kukulwe@163.com>
2018-02-02 20:46:04 +0800

    添加toast函数

---
+++ src/toast.js
@@ -0,0 +1,65 @@
+import remove from 'lodash/remove';
+export const generateToast=({
+    show,
+    hide,
+    update
+}={})=>{
+    let showCount=0;
+    let toasts=[];
+    let currentId=0;
+    const _hide=function(id){
+        remove(toasts,item=>item.id==id);
+        if(toasts.length>0){
+            const {args,id}=toasts[toasts.length-1];
+            if(currentId==id){
+                return;
+            }
+            update(args);
+            currentId=id;
+            return;
+        }
+        hide();
+    };
+
+    const _show = function(args={}){
+        const id=showCount++;
+        const {duration=0,mask,..._args}=args;
+        toasts.push({
+            id,
+            args:_args,
+            duration
+        });
+
+        setTimeout(function(){
+            _hide(id);
+        },duration);
+
+        const opt={
+            ...args,
+            duration:24*60*60*1000//忽略自动关闭功能
+        };
+        currentId=id;
+        if(toasts.length==1){
+            show(opt);
+        }else{
+            update(opt);
+        }
+        return {
+            id,
+            hide(){
+                _hide(id);
+            }
+        }
+    };
+
+    const hideAll=function(){
+        toasts=[];
+        hide();
+    };
+    return {
+        show:_show,
+        hide:_hide,
+        hideAll
+    }
+};
+export default generateToast;
---
+++ test/toast.js
@@ -0,0 +1,47 @@
+const {generateToast}=require('../lib/toast');
+
+const showToast=({
+    text,
+    duration
+})=>{
+    console.log(text);
+    console.log(duration);
+};
+
+const hideToast=()=>{
+    console.log('hide');
+};
+
+const update=showToast;
+
+
+const toast=generateToast({
+    show:showToast,
+    update,
+    hide:hideToast
+});
+
+toast.show({
+    text:'显示3秒1',
+    duration:3000
+});
+
+toast.show({
+    text:'显示10秒2',
+    duration:10000
+});
+
+toast.show({
+    text:'显示5秒3',
+    duration:5000
+});
+
+toast.show({
+    text:'显示6秒4',
+    duration:6000
+});
+
+toast.show({
+    text:'显示3秒5',
+    duration:3000
+});
